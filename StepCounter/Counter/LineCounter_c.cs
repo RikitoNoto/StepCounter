645c769018fa0dc365be919cd99488c3770fe01b
RikitoNoto <rikitonoto@gmail.com>
2022-01-21 00:02:11 +0900

    create counter branch

---
+++ StepCounter/Counter/LineCounter_c.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+
+namespace Counter
+{
+    class LineCounter_c
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+}
