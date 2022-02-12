b990da078c3d031a702b27625b8f223eaf738e3f
RikitoNoto <rikitonoto@gmail.com>
2022-02-12 16:14:05 +0900

    Add recurse search

--- StepCounter/Entry/FileExplorer_c.cs
+++ StepCounter/Entry/FileExplorer_c.cs
@@ -10,9 +10,24 @@ namespace Entry
 
         public static bool Explore(string path, Func<string, bool> callback_func)
         {
-            var file_names = from files 
-                        in Directory.EnumerateFiles(path)
-                        select files;
+            var dir_names = from dirs
+                            in Directory.EnumerateDirectories(path, "", SearchOption.AllDirectories)
+                            select dirs;
+
+            FileExplorer_c.ExploreFile(path, callback_func); // 同じ階層のディレクトリを探索
+
+            foreach(string dir_name in dir_names)
+            {
+                FileExplorer_c.ExploreFile(dir_name, callback_func);
+            }
+            return false;
+        }
+
+        private static bool ExploreFile(string path, Func<string, bool> callback_func)
+        {
+            var file_names =    from files
+                                in Directory.EnumerateFiles(path)
+                                select files;
 
 
             if (callback_func != null)
@@ -23,7 +38,9 @@ namespace Entry
                 }
 
             }
+
             return false;
         }
+
     }
 }
--- StepCounterTest/Entry/FileExplorerTest.cs
+++ StepCounterTest/Entry/FileExplorerTest.cs
@@ -16,39 +16,75 @@ namespace EntryTest
         private List<List<string>> args_EnumerateFiles = new List<List<string>>();  // EnumerateFiles メソッドが呼ばれた時の引数
         private List<string> return_value_EnumerateFiles = new List<string>();      // EnumerateFiles メソッドの返り値
 
+        private int called_count_EnumerateDirectories = 0;                                  // EnumerateDirectories メソッドが呼ばれた回数
+        private List<string> args_EnumerateDirectories_path = new List<string>();                   // EnumerateDirectories メソッドが呼ばれた時の引数path
+        private List<SearchOption> args_EnumerateDirectories_search_option = new List<SearchOption>();    // EnumerateDirectories メソッドが呼ばれた時の引数search_option
+        private Dictionary<string, List<string>> return_value_EnumerateDirectories = new Dictionary<string, List<string>>();// EnumerateDirectories メソッドの返り値
+
+
         [TestInitialize]
         public void SetUp()
         {
             this.called_count_EnumerateFiles = 0;
             this.args_EnumerateFiles.Clear();
             this.return_value_EnumerateFiles.Clear();
+
+            this.called_count_EnumerateDirectories = 0;
+            this.args_EnumerateDirectories_path.Clear();
+            this.args_EnumerateDirectories_search_option.Clear();
+            this.return_value_EnumerateDirectories.Clear();
         }
 
         /// <summary>EnumerateFilesメソッドをMockとともに呼び出す</summary>
         /// <param name="path">パス</param>
-        private void callEnumerateFilesWithMock(string path)
+        private void callExploreWithMock(string path)
         {
-            this.callEnumerateFilesWithMock(path, null);
+            this.callExploreWithMock(path, null);
         }
 
         /// <summary>EnumerateFilesメソッドをMockとともに呼び出す</summary>
         /// <param name="path">パス</param>
         /// <param name="func">コールバック関数</param>
-        private void callEnumerateFilesWithMock(string path, Func<string, bool> func)
+        private void callExploreWithMock(string path, Func<string, bool> func)
         {
-            Func<string, IEnumerable<string>> spy_func = delegate(string _path)
+            // Mockでメソッド実行
+            PoseContext.Isolate(delegate ()
+            {
+                FileExplorer_c.Explore(path, func); // Exploreメソッド呼び出し
+
+            }, this.EnumerateDirectoriesMock(path, func), this.EnumerateFilesMock(path, func));
+        }
+
+        /// <summary>EnumerateFilesのmock</summary>
+        /// <param name="path">パス</param>
+        /// <param name="func">コールバック関数</param>
+        private Shim EnumerateFilesMock(string path, Func<string, bool> func)
+        {
+            Func<string, IEnumerable<string>> spy_func = delegate (string _path)
             {
                 return this.EnumerateFilesSpy(_path); // スパイ関数の呼び出し
             };
 
             // Mockの作成
-            Shim consoleShim = Shim.Replace(() => Directory.EnumerateFiles(Is.A<string>())).With(spy_func);
+            Shim enumerate_files_mock = Shim.Replace(() => Directory.EnumerateFiles(Is.A<string>())).With(spy_func);
 
-            // Mockでメソッド実行
-            PoseContext.Isolate(delegate()
+            return enumerate_files_mock;
+        }
+
+        /// <summary>EnumerateFilesのmock</summary>
+        /// <param name="path">パス</param>
+        /// <param name="func">コールバック関数</param>
+        private Shim EnumerateDirectoriesMock(string path, Func<string, bool> func)
+        {
+            Func<string, string, SearchOption, IEnumerable<string>> spy_func = delegate (string _path, string _search_pattern, SearchOption _option)
             {
-                FileExplorer_c.Explore(path, func); // Exploreメソッド呼び出し
-            }, consoleShim);
+                return EnumerateDirectoriesSpy(_path, _search_pattern, _option); // スパイ関数の呼び出し
+            };
+
+            // Mockの作成
+            Shim enumerate_directories_mock = Shim.Replace(() => Directory.EnumerateDirectories(Is.A<string>(), Is.A<string>(), Is.A<SearchOption>())).With(spy_func);
+
+            return enumerate_directories_mock;
         }
 
         /// <summary>
@@ -72,16 +108,16 @@ namespace EntryTest
                 return false;
             };
 
-            foreach(string path in paths)
+            foreach (string path in paths)
             {
                 this.return_value_EnumerateFiles.Add(path); // mockされたEnumerateFilesの返り値リストにpathを追加
             }
 
-            this.callEnumerateFilesWithMock("src", call_back_func); // mockされたEnumerateFilesメソッドを呼び出し
+            this.callExploreWithMock("src", call_back_func); // mockされたEnumerateFilesメソッドを呼び出し
 
             Assert.AreEqual(paths.Length, call_count);         // コールバックメソッドが正しい回数呼ばれていること
 
-            for(int i=0; i < paths.Length; i++)
+            for (int i = 0; i < paths.Length; i++)
             {
                 Assert.AreEqual(paths[i], call_back_args[i]);     // コールバック関数呼び出し時の引数が正しいこと
             }
@@ -90,7 +126,7 @@ namespace EntryTest
         /// <summary>EnumerateFilesメソッドのスパイメソッド</summary>
         /// <param name="path">パス</param>
         /// <returns>パスを探索して見つかったファイルのList</returns>
-        public IEnumerable<string> EnumerateFilesSpy(string path)
+        private IEnumerable<string> EnumerateFilesSpy(string path)
         {
             this.called_count_EnumerateFiles++;     // EnumerateFilesメソッドが呼ばれた回数をインクリメント
             List<string> args = new List<string>(); // 受け取った引数用のリストを作成
@@ -100,11 +136,29 @@ namespace EntryTest
             return this.return_value_EnumerateFiles;
         }
 
+        /// <summary>EnumerateDirectoriesメソッドのスパイメソッド</summary>
+        /// <param name="path">パス</param>
+        /// <returns>パスを探索して見つかったディレクトリのList</returns>
+        private IEnumerable<string> EnumerateDirectoriesSpy(string path, string search_pattern, SearchOption option)
+        {
+            this.called_count_EnumerateDirectories++;     // EnumerateFilesメソッドが呼ばれた回数をインクリメント
+            this.args_EnumerateDirectories_path.Add(path); // pathを追加
+            this.args_EnumerateDirectories_search_option.Add(option);
+            System.Console.WriteLine(this.return_value_EnumerateDirectories.ContainsKey(path));
+
+            if (this.return_value_EnumerateDirectories.ContainsKey(path))
+            {
+                return this.return_value_EnumerateDirectories[path];
+            }
+
+            return new List<string>();
+        }
+
         [TestMethod, TestCategory("Explore")]
         [Description("一つのディレクトリの探索ができること")]
         public void 引数で渡した空文字列でEnumerateFilesを呼び出すこと()
         {
-            this.callEnumerateFilesWithMock("");
+            this.callExploreWithMock("");
 
             Assert.IsTrue(this.called_count_EnumerateFiles > 0);    // 一度でもEnumerateFilesメソッドが呼ばれていること
             Assert.AreEqual(this.args_EnumerateFiles[0][0], "");    // 引数は空の文字列であること
@@ -114,7 +168,7 @@ namespace EntryTest
         [Description("一つのディレクトリの探索ができること")]
         public void 引数で渡したパスでEnumerateFilesを呼び出すこと()
         {
-            this.callEnumerateFilesWithMock("src");
+            this.callExploreWithMock("src");
 
             Assert.IsTrue(this.called_count_EnumerateFiles > 0);       // 一度でもEnumerateFilesメソッドが呼ばれていること
             Assert.AreEqual(this.args_EnumerateFiles[0][0], "src");    // 引数は空の文字列であること
@@ -133,5 +187,115 @@ namespace EntryTest
         {
             this.checkCallBackFunc_Explore("file1.c", "file2.c");
         }
+
+        [TestMethod, TestCategory("Explore")]
+        [Description("ディレクトリの探索が再帰的にできること")]
+        public void 引数で渡した空文字列でEnumerateDirectoriesを呼び出すこと()
+        {
+            this.callExploreWithMock("");
+
+            Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+            Assert.AreEqual(this.args_EnumerateDirectories_path[0], "");
+        }
+
+        [TestMethod, TestCategory("Explore")]
+        [Description("ディレクトリの探索が再帰的にできること")]
+        public void 引数で渡した文字列でEnumerateDirectoriesを呼び出すこと()
+        {
+            this.callExploreWithMock("src");
+
+            Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+            Assert.AreEqual(this.args_EnumerateDirectories_path[0], "src");
+        }
+
+        //NOTE: 再帰検索はEnumerateDirectories のオプションで設定できる。
+        //      オプションが正しく渡されるかをテストする。
+
+        [TestMethod, TestCategory("Explore")]
+        [Description("ディレクトリの探索が再帰的にできること")]
+        public void EnumerateFilesを再帰オプション付きで呼び出すこと()
+        {
+            this.callExploreWithMock("src");
+
+            Assert.IsTrue(this.called_count_EnumerateFiles == 1);
+            Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+            Assert.AreEqual(this.args_EnumerateDirectories_path[0], "src");
+            Assert.AreEqual(this.args_EnumerateDirectories_search_option[0], SearchOption.AllDirectories);
+        }
+
+        //[TestMethod, TestCategory("Explore")]
+        //[Description("ディレクトリの探索が再帰的にできること")]
+        //public void 子ディレクトリが存在しないときEnumerateFilesを1回呼び出すこと()
+        //{
+        //    this.callExploreWithMock("src");
+
+        //    Assert.IsTrue(this.called_count_EnumerateFiles == 1);
+        //    Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+        //    Assert.AreEqual(this.args_EnumerateDirectories_path[0][0], "src");
+        //}
+
+        //[TestMethod, TestCategory("Explore")]
+        //[Description("ディレクトリの探索が再帰的にできること")]
+        //public void 子ディレクトリが1つ存在するときEnumerateFilesを2回呼び出すこと()
+        //{
+        //    List<string> children = new List<string>();
+        //    children.Add("child_dir");
+        //    this.return_value_EnumerateDirectories["src"] = children;
+
+        //    this.callExploreWithMock("src");
+
+        //    Assert.IsTrue(this.called_count_EnumerateFiles == 2);
+        //    Assert.AreEqual(this.args_EnumerateFiles[0][0], "src");
+        //    Assert.AreEqual(this.args_EnumerateFiles[1][0], "child_dir");
+        //    Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+        //    Assert.AreEqual(this.args_EnumerateDirectories_path[0][0], "src");
+
+        //}
+
+        //[TestMethod, TestCategory("Explore")]
+        //[Description("ディレクトリの探索が再帰的にできること")]
+        //public void 子ディレクトリが2つ存在するときEnumerateFilesを3回呼び出すこと()
+        //{
+        //    List<string> children = new List<string>();
+        //    children.Add("child_dir1");
+        //    children.Add("child_dir2");
+        //    this.return_value_EnumerateDirectories["src"] = children;
+
+        //    this.callExploreWithMock("src");
+
+        //    Assert.IsTrue(this.called_count_EnumerateFiles == 3);
+        //    Assert.AreEqual(this.args_EnumerateFiles[0][0], "src");
+        //    Assert.AreEqual(this.args_EnumerateFiles[1][0], "child_dir1");
+        //    Assert.AreEqual(this.args_EnumerateFiles[2][0], "child_dir2");
+        //    Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+        //    Assert.AreEqual(this.args_EnumerateDirectories_path[0][0], "src");
+        //}
+
+        //[TestMethod, TestCategory("Explore")]
+        //[Description("ディレクトリの探索が再帰的にできること")]
+        //public void 孫ディレクトリが1つ存在するときEnumerateFilesを3回呼び出すこと()
+        //{
+        //    List<string> children = new List<string>();
+        //    children.Add("child_dir1");
+        //    this.return_value_EnumerateDirectories["src"] = children;
+
+        //    List<string> grand_children = new List<string>();
+        //    grand_children.Add("grand_child_dir1");
+        //    this.return_value_EnumerateDirectories["child_dir1"] = grand_children;
+
+        //    this.callExploreWithMock("src");
+
+        //    Assert.IsTrue(this.called_count_EnumerateFiles == 3);
+        //    Assert.AreEqual(this.args_EnumerateFiles[0][0], "src");
+        //    Assert.AreEqual(this.args_EnumerateFiles[1][0], "child_dir1");
+        //    Assert.AreEqual(this.args_EnumerateFiles[2][0], "grand_child_dir1");
+        //    Assert.IsTrue(this.called_count_EnumerateDirectories > 0);
+        //    Assert.AreEqual(this.args_EnumerateDirectories_path[0][0], "src");
+        //}
+
+        //TODO: Enumerateメソッドの例外 「パスが空」を補足
+        //TODO: Enumerateメソッドの例外 「存在しないパス」を補足
+        //TODO: ファイル名ルール
+        //TODO: ディレクトリ名ルール
     }
 }
